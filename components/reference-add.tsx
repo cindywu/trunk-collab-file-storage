319689b1b3a8450f2c13db22e6ad5890c29ca146
cindy <ciindy.wu@gmail.com>
2021-09-27 18:05:52 -1000

    allow source file upload from reference add modal [incomplete]

--- components/reference-add.tsx
+++ components/reference-add.tsx
@@ -1,26 +1,32 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, ChangeEvent } from 'react'
 import styles from './reference-add.module.css'
 import { useReferences } from './reference-provider'
 import { v4 as uuidv4 } from 'uuid'
+import UploadButton from './upload-button'
 
 export default function ReferenceAdd({ rep }: any) {
   const {
     showReferenceAdd,
     handleReferenceAdd,
-    handleShowReferenceAdd
+    handleShowReferenceAdd,
+    session
   } = useReferences()
+
+  const [uploading, setUploading] = useState(false)
+  const [sourceUrl, setSourceUrl] = useState('')
 
   const nameRef = useRef<HTMLInputElement>(null)
   const parentRef = useRef<HTMLInputElement>(null)
   const titleRef = useRef<HTMLTextAreaElement>(null)
+  const newRefId = uuidv4()
 
   function handleSaveReference(){
     const date = new Date()
     const myDate = (date.toLocaleString('default', { month: 'short'})) + " " + date.getUTCDate()
 
     const newReference = {
-      id: uuidv4(),
-      source_url: '',
+      id: newRefId,
+      source_url: sourceUrl,
       name: nameRef.current ? nameRef.current.value : '',
       parent: parentRef.current ? parentRef.current.value : '',
       date: myDate,
@@ -41,6 +47,66 @@ export default function ReferenceAdd({ rep }: any) {
       ],
     }
     handleReferenceAdd(newReference)
+    // uploadAvatar(stuff, newFileID) // upload file to supabase
+  }
+
+  function idbOK() {
+    return "indexedDB" in window
+  }
+
+  function handleUpload(event:ChangeEvent<HTMLInputElement>) {
+    const stuff = event
+    setUploading(true)
+
+    if (!event.target.files || event.target.files.length == 0) {
+      throw 'You must select an image to upload.'
+    }
+
+    let file = event.target.files[0]
+
+    if (!idbOK()) return
+
+    let openRequest = indexedDB.open('trunk_idb1', 1)
+
+    openRequest.onupgradeneeded = function(event: any) {
+      let thisDB = event.target.result
+
+      if (!thisDB.objectStoreNames.contains('sources')) {
+        thisDB.createObjectStore('sources', { keyPath: 'id' })
+      }
+    }
+
+    openRequest.onsuccess = function(event: any) {
+      let db = event.target.result
+      let tx = db.transaction(['sources'], 'readwrite')
+      let store = tx.objectStore('sources')
+
+      let fileID = uuidv4()
+      let refID = newRefId
+      let newFileID = `${refID}/${fileID}`
+
+      setSourceUrl(newFileID)
+
+      let newFile = {
+        id: newFileID,
+        file: file,
+      }
+
+      let request = store.add(newFile)
+
+      request.onerror = function(event: any) {
+        console.log('error', event.target.error.name)
+      }
+
+      request.onsuccess = function(event: any) {
+
+        setUploading(false)
+      }
+    }
+
+    openRequest.onerror = function(event:any) {
+      console.dir(event)
+    }
   }
 
   return (
@@ -89,7 +155,18 @@ export default function ReferenceAdd({ rep }: any) {
         <div className={styles.buttonContainer}>
           <div className={styles.left}>
             <button className="btn btn--secondary">
-              Attach file
+              <div className={styles.sourceFileContainer}>
+                {sourceUrl &&
+                  <div>
+                    🗂 Source
+                  </div>
+                }
+                <UploadButton
+                  onUpload={handleUpload}
+                  loading={uploading}
+                  sourceUrl={sourceUrl}
+                />
+              </div>
             </button>
           </div>
           <div className={styles.right}>
